da40f423e950b3c0869fd71b65a2bb81fdd649b1
Øyvind Ege <oyvind.ege@pm.me>
2023-11-29 12:58:26 +0100

    Added allergies toggle

--- src/routes/eventer/[eventId]/+page.svelte
+++ src/routes/eventer/[eventId]/+page.svelte
@@ -2,12 +2,16 @@
 	import Textfield from '@smui/textfield';
 	import Button from '@smui/button';
 	import Chip, { Set, Text } from '@smui/chips';
+	import Checkbox from '@smui/checkbox';
+	import FormField from '@smui/form-field';
 
 	import type { PageData } from './$types';
 
 	let navn: string = '';
 	let org: string = '';
 	let allergier: string[] = [];
+
+	let showAllergier: boolean = false;
 
 	export let data: PageData;
 </script>
@@ -32,10 +36,17 @@
 			variant="outlined"
 		/>
 		<div class="allergiListe">
-			<h2>Allergiliste</h2>
-			<Set chips={data.allergiListe.sort()} let:chip filter bind:selected={allergier}>
-				<Chip {chip}><Text>{chip}</Text></Chip>
-			</Set>
+			<FormField>
+				<Checkbox bind:checked={showAllergier} />
+				<span slot="label">Jeg har allergier</span>
+			</FormField>
+
+			{#if showAllergier}
+				<h2>Allergiliste</h2>
+				<Set chips={data.allergiListe.sort()} let:chip filter bind:selected={allergier}>
+					<Chip {chip}><Text>{chip}</Text></Chip>
+				</Set>
+			{/if}
 		</div>
 		<Button variant="raised">Meld deg på</Button>
 	</form>
@@ -55,7 +66,7 @@
 	}
 
 	.allergiListe {
-		max-width: 40em;
+		width: 40em;
 		margin-bottom: 5%;
 	}
 </style>
