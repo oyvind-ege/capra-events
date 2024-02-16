d1165e2b3dda717c49ab5b23d6df246abfeee47b
mariussd <marius.s.dreyer@gmail.com>
2024-02-16 10:40:50 +0100

    chore: remove prisma

--- src/lib/server/client.ts
+++ src/lib/server/client.ts
@@ -1,7 +1,1 @@
-import { PrismaClient } from '@prisma/client';
 
-/* A common Prisma Client, to avoid instatiating Prisma clients all over the place. */
-
-const prisma = new PrismaClient();
-
-export default prisma;
