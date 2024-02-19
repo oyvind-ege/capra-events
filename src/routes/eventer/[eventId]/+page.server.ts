1fdfe838122b17a75c2a86159a10ba25cdff840d
Øyvind Ege <oyvind.ege@pm.me>
2024-02-19 08:53:48 +0100

    chore: removed unused database functions, files and references

--- src/routes/eventer/[eventId]/+page.server.ts
+++ src/routes/eventer/[eventId]/+page.server.ts
@@ -1,11 +1,3 @@
-import { getAllergiListe, getEventInformasjon } from '$lib/server/database';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
-	const eventData = await getEventInformasjon(Number(params.eventId));
-	const allergiListe = (await getAllergiListe()).map((allergiObject) => allergiObject.navn);
-	return {
-		eventData,
-		allergiListe
-	};
-};
+export const load: PageServerLoad = async () => {};
